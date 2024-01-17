5045586f933fc6b0fe23934725e2624843be3767
Wesley <wjapajoe@ziggo.nl>
2024-01-17 04:38:25 -0300

    Add Gravy.hpp

---
+++ include/Gravy.hpp
@@ -0,0 +1,41 @@
+#ifndef GRAVY_GRAVY_HPP
+#define GRAVY_GRAVY_HPP
+
+#include "System/String.hpp"
+#include "System/BitConverter.hpp"
+#include "System/DateTime.hpp"
+#include "System/Collections/ConcurrentQueue.hpp"
+#include "System/Signal.hpp"
+#include "System/HashCode.hpp"
+#include "System/BinaryStream.hpp"
+#include "System/Math.hpp"
+#include "System/StringBuilder.hpp"
+#include "System/Net/HttpClient.hpp"
+#include "System/Net/SslException.hpp"
+#include "System/Net/Socket.hpp"
+#include "System/Net/NetworkStream.hpp"
+#include "System/Net/SslStream.hpp"
+#include "System/Net/TcpClient.hpp"
+#include "System/Net/HostInfo.hpp"
+#include "System/Net/TcpListener.hpp"
+#include "System/Net/SslContext.hpp"
+#include "System/Text/Tokenizer.hpp"
+#include "System/Guid.hpp"
+#include "System/URI.hpp"
+#include "System/Console.hpp"
+#include "System/Random.hpp"
+#include "System/IO/FileStream.hpp"
+#include "System/IO/FileInfo.hpp"
+#include "System/IO/MemoryStream.hpp"
+#include "System/IO/FileTimeInfo.hpp"
+#include "System/IO/Stream.hpp"
+#include "System/IO/Directory.hpp"
+#include "System/IO/File.hpp"
+
+using namespace Gravy::System;
+using namespace Gravy::System::Collections;
+using namespace Gravy::System::IO;
+using namespace Gravy::System::Net;
+using namespace Gravy::System::Text;
+
+#endif
